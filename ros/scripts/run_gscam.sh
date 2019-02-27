5c4c3330a3caf430cc04a1f85c83dda95d373bfc
Julian True <julian.allan.true@gmail.com>
2019-02-27 00:26:46 +0000

    Switched to /dev/video1 input for usb device

--- ros/scripts/run_gscam.sh
+++ ros/scripts/run_gscam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd ${CATKIN_WS}/
-export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw, width=640, height=360 ! ffmpegcolor space"
+cd ${CATKIN_WS}
+export GSCAM_CONFIG="v4l2src device=/dev/video1 ! video/x-raw, width=640, height=360 ! ffmpegcolor space"
 rosrun gscam gscam
